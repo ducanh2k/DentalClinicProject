a5a5f8e58880b72a1454f024037de99af4091803
đức anh trần <tdanhaltt18@gmail.com>
2024-02-25 22:28:24 +0700

    update table

--- DentalClinicProject/src/App.vue
+++ DentalClinicProject/src/App.vue
@@ -5,10 +5,10 @@
 </template>
 
 <script>
-import "@fortawesome/fontawesome-free/css/all.css";
-// import HomePage from "./layout/User/HomePage.vue";
-import AdminVue from "./Admin.vue";
-import Service from "./layout/Admin/ServiceManagement/Service.vue";
+import '@fortawesome/fontawesome-free/css/all.css'
+import HomePage from './layout/Admin/HomePage.vue';
+import Service from './layout/Admin/ServiceManagement/Service.vue';
+// import AdminVue from './Admin.vue';
 export default {
   name: "App",
   components: {
